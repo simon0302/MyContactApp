1ed7bcf5c126d173e14d3420afa22d462ea64a13
simon0302 <sx0302@yahoo.com>
2017-05-08 10:55:04 -0700

    added test comment
    added comment

--- app/src/main/java/com/example/simon/mycontactapplication/MainActivity.java
+++ app/src/main/java/com/example/simon/mycontactapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //testcomment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
